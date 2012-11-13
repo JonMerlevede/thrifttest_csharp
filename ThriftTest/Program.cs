7f76a5c61bf5275d1110184151e5c8ab2c2e6468
Jonathan Merlevede <jonathan@merlevede.eu>
2012-11-13 23:10:26 +0100

    Initial commit.

---
+++ ThriftTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var socket = new TSocket("localhost", 7911);
+                var transport = new TFramedTransport(socket);
+                var protocol = new TBinaryProtocol(transport);
+                var client = new Tester.Client(protocol);
+                transport.Open();
+                var test = new TTest {Mekker = "dit is een test\n", NumberOfTimes = 10};
+                Console.WriteLine(client.makeString(test));
+                transport.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Pres key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
